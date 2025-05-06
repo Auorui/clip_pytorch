4f9a714c6a21dd5bb523d11b3f0554d18451ed63
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2025-05-06 18:32:33 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import cv2
 import torch
 from PIL import Image
 import numpy as np
@@ -27,6 +28,7 @@ class Predict(nn.Module):
         image = self.preprocess(image_path).float().to(self.device)
         text = tokenize(text_language).to(self.device)
         self.model.eval()
+        original_image = cv2.imread(image_path)
         with torch.no_grad():
             logits_per_image, logits_per_text = self.model(image, text)
             probs = logits_per_image.softmax(dim=-1).cpu().numpy()
@@ -34,16 +36,28 @@ class Predict(nn.Module):
             print("Label probs:", probs)
             for i in range(image.shape[0]):  # batch
                 id = idx[i]
-                print('{}:\t{}'.format(text_language[id], probs[i, id]))
-                print('image {}:\t{}'.format(i, [v for v in zip(text_language, probs[i])]))
+                prediction = f"{text_language[id]}: {probs[i, id]:.2f}"
 
+                (text_width, text_height), baseline = cv2.getTextSize(prediction, cv2.FONT_HERSHEY_SIMPLEX, .8, 2)
+                text_x = (original_image.shape[1] - text_width) // 2
+                text_y = original_image.shape[0] - 10
+                cv2.putText(original_image, prediction, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, .8,
+                            (255, 255, 255), 2, cv2.LINE_AA)
+
+                print(prediction)
+                print('image {}:\t{}'.format(i, [v for v in zip(text_language, probs[i])]))
+                cv2.imshow("predict opencv image", original_image)
+                cv2.waitKey(0)
 
 if __name__=="__main__":
-    model_pth_path = r"E:\PythonProject\clip_pytorch\logs\2025_05_06_16_36_12\weights\best_epoch_weights.pth"
-    image_path = r"E:\PythonProject\clip_pytorch\flickr8k\images\35506150_cbdb630f4f.jpg"
-    text_language = ["A man in a red jacket is sitting on a bench whilst cooking a meal",
-                     "A greyhound walks in the rain through a large puddle",
-                     "A group of people ride in a race",
-                     "A cyclist is performing a jump near to a railing and a brick wall"]
-    model_predict = Predict(model_pth_path)
-    model_predict(image_path, text_language)
+    while True:
+        model_pth_path = r"E:\PythonProject\clip_pytorch\logs\2025_05_06_16_36_12\weights\best_epoch_weights.pth"
+        image_path = input("请输入图像路径：")
+        # E:\PythonProject\clip_pytorch\flickr8k\images\35506150_cbdb630f4f.jpg
+        # E:\PythonProject\clip_pytorch\flickr8k\images\3682277595_55f8b16975.jpg
+        text_language = ["A man in a red jacket is sitting on a bench whilst cooking a meal",
+                         "A greyhound walks in the rain through a large puddle",
+                         "A group of people ride in a race",
+                         "A cyclist is performing a jump near to a railing and a brick wall"]
+        model_predict = Predict(model_pth_path)
+        model_predict(image_path, text_language)
